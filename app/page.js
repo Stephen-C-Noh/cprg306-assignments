f1d5be2357ca9104ab2bf41242ba6b7f3c856c8d
Stephen C Noh <stephen.c.noh@gmail.com>
2026-02-02 07:21:56 -0700

    Refactor page layout to use list elements for week assignments

--- app/page.js
+++ app/page.js
@@ -6,19 +6,20 @@ export default function Page() {
       <header>
         <h1>CRPG306: Web Development 2 - Assignments</h1>
       </header>
-      <p>
+      <ul className="list-none space-y-1">
+      <li>
         <strong>Week 2:</strong>{' '}
         <Link href="/week-2">
           Week 2
         </Link>
-      </p>
-      <p>
+      </li>
+      <li>
         <strong>Week 3:</strong>{' '}
         <Link href="/week-3">
           Week 3
         </Link>
-      </p>
-
+      </li>
+    </ul>
     </main>
   );
 }
